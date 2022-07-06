f2ee674e8905dd33636829d80e4f5d588a7caade
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-07-06 19:21:07 +0500

    Contract write and configuration

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7771891dDC4bc6B2fc251560aA8cBD41e44D18B3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
